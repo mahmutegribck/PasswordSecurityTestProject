ce7a79d4618cd5bc87fca1dbb62c506ac50320c6
mahmutegribck <104262911+mahmutegribck@users.noreply.github.com>
2022-05-07 03:35:05 +0300

    Password Security Test Project

---
+++ PasswordSecurityTestProject/PasswordSecurityTestProject/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace PasswordSecurityTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string sifre;
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.Write("Sifre Giriniz : ");
+                sifre = Console.ReadLine();
+
+                int boslukSayisi = sifre.Length - Regex.Replace(sifre, " ", "").Length;  //Sifredeki bosluk kontrolu yapilir ve bosluk sayisi bulunur
+
+                if (boslukSayisi > 0)       //Sifrede bosluk varsa tekrar sifre istenmesi saglanir
+                {
+                    Console.WriteLine("\nBosluk Oldugu Icin Sifre Gecersiz Lutfen Tekrar Sifre Giriniz!!\n");
+                    continue;
+                }
+                else
+                {
+                    int buyukHarafPuani = SifrePuanlama.BuyukHarfPaun(sifre);
+                    int kucukHarfPuani = SifrePuanlama.KucukHarfPuan(sifre);
+                    int rakamPuani = SifrePuanlama.RakamPuan(sifre);
+                    int sembolPuani = SifrePuanlama.SembolPuan(sifre);
+                    int sifrePuani = buyukHarafPuani + kucukHarfPuani + rakamPuani + sembolPuani;
+
+                    if (sifre.Length < 9)
+                    {
+                        Console.WriteLine("\nSifre Dokuz Karakterden Kisa Oldugu Icin Kabul Edilemez Lutfen Tekrar Sifre Giriniz!!\n");
+                        continue;
+                    }
+                    
+                    if (sifre.Length == 9 || sifre.Length > 9)
+                    {
+                        if (sifre.Length == 9)
+                        {
+                            sifrePuani += 10;            //Sifre karakter sayisinin dokuza esit olmasi durumunda sifre puanina on puan eklenir
+                        }
+
+                        if (buyukHarafPuani == 0)     //Sifrede buyuk harf olmamasi durumunda tekrar sifre istenmesi saglanir
+                        {
+                            Console.WriteLine("\nSifre Buyuk Harf Icermedigi Icin Kabul Edilemez Lutfen Tekrar Sifre Giriniz!!\n");
+                            continue;
+                        }
+                        if (kucukHarfPuani == 0)    //Sifrede kucuk harf olmamasi durumunda tekrar sifre istenmesi saglanir
+                        {
+                            Console.WriteLine("\nSifre Kucuk Harf Icermedigi Icin Kabul Edilemez Lutfen Tekrar Sifre Giriniz!!\n");
+                            continue;
+                        }
+                        if (rakamPuani == 0)        //Sifrede rakam olmamasi durumunda tekrar sifre istenmesi saglanir
+                        {
+                            Console.WriteLine("\nSifre Rakam Icermedigi Icin Kabul Edilemez Lutfen Tekrar Sifre Giriniz!!\n");
+                            continue;
+                        }
+                        if (sembolPuani == 0)       //Sifrede sembol olmamasi durumunda tekrar sifre istenmesi saglanir
+                        {
+                            Console.WriteLine("\nSifre Sembol Icermedigi Icin Kabul Edilemez Lutfen Tekrar Sifre Giriniz!!\n");
+                            continue;
+                        }
+
+                        if (sifrePuani < 70)
+                        {
+                            Console.WriteLine("\nSifre Gecersiz Lutfen Tekrar Deneyiniz\n");
+                            Console.WriteLine("Sifre Puani : {0}", sifrePuani);
+                            Console.WriteLine("Buyuk Harf Sayisi : {0}", SifrePuanlama.buyukHarfSayisi);
+                            Console.WriteLine("Kucuk Harf Sayisi : {0}", SifrePuanlama.kucukHarfSayisi);
+                            Console.WriteLine("Rakam Sayisi : {0}", SifrePuanlama.rakamSayisi);
+                            Console.WriteLine("Sembol Sayisi : {0}\n", SifrePuanlama.sembolSayisi);
+                        }
+                        if (sifrePuani >= 70 && sifrePuani < 90)
+                        {
+                            Console.WriteLine("\nSifre Kabul Edilir\n");
+                            Console.WriteLine("Sifre Puani : {0}", sifrePuani);
+                            Console.WriteLine("Buyuk Harf Sayisi : {0}", SifrePuanlama.buyukHarfSayisi);
+                            Console.WriteLine("Kucuk Harf Sayisi : {0}", SifrePuanlama.kucukHarfSayisi);
+                            Console.WriteLine("Rakam Sayisi : {0}", SifrePuanlama.rakamSayisi);
+                            Console.WriteLine("Sembol Sayisi : {0}\n", SifrePuanlama.sembolSayisi);
+                        }
+                        if (sifrePuani >= 90 && sifrePuani <= 100)
+                        {
+                            Console.WriteLine("\nSifre Kabul Edilir Ve Guclu\n");
+                            Console.WriteLine("Sifre Puani : {0}", sifrePuani);
+                            Console.WriteLine("Buyuk Harf Sayisi : {0}", SifrePuanlama.buyukHarfSayisi);
+                            Console.WriteLine("Kucuk Harf Sayisi : {0}", SifrePuanlama.kucukHarfSayisi);
+                            Console.WriteLine("Rakam Sayisi : {0}", SifrePuanlama.rakamSayisi);
+                            Console.WriteLine("Sembol Sayisi : {0}\n", SifrePuanlama.sembolSayisi);
+                        }
+                        if(sifrePuani > 100)
+                        {
+                            Console.WriteLine("\nSifre Kabul Edilir Ve Cok Guclu\n");
+                            Console.WriteLine("Sifre Puani : {0}", sifrePuani);
+                            Console.WriteLine("Buyuk Harf Sayisi : {0}", SifrePuanlama.buyukHarfSayisi);
+                            Console.WriteLine("Kucuk Harf Sayisi : {0}", SifrePuanlama.kucukHarfSayisi);
+                            Console.WriteLine("Rakam Sayisi : {0}", SifrePuanlama.rakamSayisi);
+                            Console.WriteLine("Sembol Sayisi : {0}\n", SifrePuanlama.sembolSayisi);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ PasswordSecurityTestProject/PasswordSecurityTestProject/SifrePuanlama.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace PasswordSecurityTestProject
+{
+    public static class SifrePuanlama
+    {
+        public static int puan = 0;                     // Sifre puanini tutacak degisken tanimlandi
+        public static int buyukHarfSayisi = 0;          // Buyuk harf sayisini tutacak degisken tanimlandi
+        public static int kucukHarfSayisi = 0;          // Kucuk harf sayisini tutacak degisken tanimlandi
+        public static int rakamSayisi = 0;              // Rakam sayisini tutacak degisken tanimlandi
+        public static int sembolSayisi = 0;             // Sembol sayisini tutacak degisken tanimlandi
+        public static int BuyukHarfPaun(string sifre)       //Sifre icindeki buyuk harfleri bulan ve puan donduren fonksiyon tanimlandi
+        {
+            buyukHarfSayisi = sifre.Length - Regex.Replace(sifre, "[A-Z]|[ŞİÇÖÜĞ]", "").Length;       //Sifrenin uzunlugundan icinden aranan karakterlerin cikarildigi sifrenin uzunlugu cikarilarak istenen karakterlerin sayisi bulunur   
+            puan = Math.Min(2, buyukHarfSayisi) * 10;       //Bulunan buyuk harf sayisina gore en kucuk sayi secilerek 10 ile carpilir ve puan elde edilir
+            return puan;        
+        }
+        public static int KucukHarfPuan(string sifre)       //Sifre icindeki kucuk harfleri bulan ve puan donduren fonksiyon tanimlandi
+        {
+            kucukHarfSayisi = sifre.Length - Regex.Replace(sifre, "[a-z]|[şıçöüğ]", "").Length;
+            puan = Math.Min(2, kucukHarfSayisi) * 10;       //Bulunan kucuk harf sayisina gore en kucuk sayi secilerek 10 ile carpilir ve puan elde edilir
+            return puan;
+        }
+        public static int RakamPuan(string sifre)       //Sifre icindeki rakamlari bulan ve puan donduren fonksiyon tanimlandi
+        {
+            rakamSayisi = sifre.Length - Regex.Replace(sifre, "[0-9]", "").Length;
+            puan = Math.Min(2, rakamSayisi) * 10;       //Bulunan rakam sayisina gore en kucuk sayi secilerek 10 ile carpilir ve puan elde edilir
+            return puan;
+        }
+        public static int SembolPuan(string sifre)      //Sifre icindeki sembolleri bulan ve puan donduren fonksiyon tanimlandi
+        {
+            sembolSayisi = Regex.Replace(sifre, "[a-zA-Z0-9]", "").Length;      //Sifreden belirtilen karakterler ve rakamlar cikarilarak sifredeki sembol sayisi bulunur
+            puan = sembolSayisi * 10;   
+            return puan;
+        }
+    }
+}
